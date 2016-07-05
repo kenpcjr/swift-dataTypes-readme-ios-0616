7dd0986a41e008a7c770a3bdfadddaaa960f847e
kenpcjr <kennethcooke@me.com>
2016-07-05 13:15:46 -0400

    Done.

---
+++ Data Types- Strings and Integers-KC.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String =  "Jupiter"
+
+let sixthPlanet: String = "Saturn"
+
+let seventhPlanet = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) orbitting \(sixthPlanet)")
